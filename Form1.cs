a33fb091fc79460505dbabcbbc9633d9d5de3db6
KungSimon <simon.naslund@hotmail.com>
2023-11-28 11:55:41 +0100

    Final

--- Form1.cs
+++ Form1.cs
@@ -191,94 +191,6 @@ namespace CarRegistry
         {
             LoadDataFromFile();
         }
-
-        //private void searchVihiclesButtonis_Click(object sender, EventArgs e)
-        //{
-        //    string searchTerm = searchVehiclesTextBoxis.Text;
-        //
-        //    if (!string.IsNullOrEmpty(searchTerm))
-        //    {
-        //        Cars foundCar = carList.FirstOrDefault(car => car.LicensePlateNumber == searchTerm);
-        //
-        //        if (foundCar != null)
-        //        {
-        //            Vehicle vehicle = new Vehicle(foundCar);
-        //            vehicle.Show();
-        //
-        //        }
-        //        else
-        //        {
-        //            MessageBox.Show("No car matched your search.", "Not Found", MessageBoxButtons.OK, MessageBoxIcon.Information);
-        //        }
-        //    }
-        //    else
-        //    {
-        //        MessageBox.Show("Please enter a search term.", "Empty Search", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-        //    }
-        //    searchVehiclesTextBoxis.Clear();
-        //}
-
-        //private void button1_Click(object sender, EventArgs e)
-        //{
-        //    try
-        //    {
-        //        carList.Clear();
-        //        CarsListBox.Items.Clear();
-        //
-        //        if (File.Exists(path))
-        //        {
-        //            using (StreamReader reader = new StreamReader(path))
-        //            {
-        //                string line = reader.ReadLine();
-        //                while (line != null)
-        //                {
-        //                    try
-        //                    {
-        //                        string[] strings = line.Split(",");
-        //
-        //                        string Brand = strings[0];
-        //                        string Model = strings[1];
-        //                        string Color = strings[2];
-        //                        int Year = Convert.ToInt32(strings[3]);
-        //                        int LastInspectionDate = Convert.ToInt32(strings[4]);
-        //                        string licensePlateNumber = strings[5];
-        //                        bool electricCar = Convert.ToBoolean(strings[6]);
-        //
-        //                        Cars cars = new Cars(Brand, Model, Color, Year, LastInspectionDate, licensePlateNumber, electricCar);
-        //                        carList.Add(cars);
-        //                        CarsListBox.Items.Add(cars.GetInfo());
-        //
-        //                        line = reader.ReadLine();
-        //                    }
-        //                    catch (Exception ex)
-        //                    {
-        //
-        //                    }
-        //                }
-        //            }
-        //        }
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        MessageBox.Show("Error loading data: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-        //    }
-        //}
-
-        //private void CarsListBox_SelectedIndexChanged(object sender, EventArgs e)
-        //{
-        //    int index = CarsListBox.SelectedIndex;
-        //    if (index >= 0)
-        //    {
-        //        carBrandTextBox.Text = carList[index].Brand;
-        //        carModelTextBox.Text = carList[index].Model;
-        //        carColorTextBox.Text = carList[index].Color;
-        //        carYearComboBox.SelectedItem = carList[index].CarYear;
-        //        inspectionYearComboBox.SelectedItem = carList[index].LastCarInspection;
-        //        licancePlatetextBox.Text = carList[index].LicensePlateNumber;
-        //        isElectricCheckBox.Checked = carList[index].IsElectric;
-        //    }
-        //}
-
         private void searchVihiclesButtonis_Click_1(object sender, EventArgs e)
         {
             string searchTerm = searchVehiclesTextBoxis.Text;
@@ -403,7 +315,7 @@ namespace CarRegistry
 
         private void SearchCarsButton_Click_1(object sender, EventArgs e)
         {
-            string searchTerm = licancePlatetextBox.Text.Trim(); // Ta bort eventuella extra mellanslag
+            string searchTerm = licancePlatetextBox.Text.Trim(); 
 
             if (!string.IsNullOrEmpty(searchTerm))
             {
